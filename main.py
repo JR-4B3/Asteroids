28ef3dd422bc7ff3f482bfc0d441757854c6805d
JR-4B3 <rabe.fynn@startmail.com>
2025-11-14 15:21:57 +0100

    Add project setup and first lesson on module imports

---
+++ main.py
@@ -0,0 +1,9 @@
+import pygame
+from constant import SCREEN_HEIGHT, SCREEN_WIDTH
+
+def main():
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(f"Screen Width: {SCREEN_WIDTH} \n Screen Hight: {SCREEN_HEIGHT}")
+
+if __name__ == "__main__":
+    main()
