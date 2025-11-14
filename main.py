9f6c1ad41c55c066918a6e088cd8e16cc19eff29
JR-4B3 <rabe.fynn@startmail.com>
2025-11-14 21:13:09 +0100

    Add Basic Game loop with black Screen

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 import pygame
-from constant import SCREEN_HEIGHT, SCREEN_WIDTH
+from logger import log_state
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 
-def main():
+def main(): 
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
-    print(f"Screen Width: {SCREEN_WIDTH} \n Screen Hight: {SCREEN_HEIGHT}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
+
 
 if __name__ == "__main__":
     main()
