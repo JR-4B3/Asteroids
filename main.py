ad4f6bbdced1027014d701f689f555755d1deac9
JR-4B3 <rabe.fynn@startmail.com>
2025-11-14 23:02:59 +0100

    Add Palyer drawn to Screen

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from logger import log_state
-from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from player import Player
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH, PLAYER_RADIUS
 
 def main(): 
     pygame.init()
@@ -8,13 +9,18 @@ def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
